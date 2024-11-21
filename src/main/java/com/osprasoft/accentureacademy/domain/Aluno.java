f746933346a754e385e5cd4f71b10485040801f0
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-11-21 07:51:12 -0300

    Configuração do hibernate

--- src/main/java/com/osprasoft/accentureacademy/domain/Aluno.java
+++ src/main/java/com/osprasoft/accentureacademy/domain/Aluno.java
@@ -1,8 +1,19 @@
 package com.osprasoft.accentureacademy.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Aluno {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
--- src/main/java/com/osprasoft/accentureacademy/domain/Curso.java
+++ src/main/java/com/osprasoft/accentureacademy/domain/Curso.java
@@ -1,8 +1,18 @@
 package com.osprasoft.accentureacademy.domain;
 
+import java.io.Serializable;
 import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-public class Curso {
+@Entity
+public class Curso implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
