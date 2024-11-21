da91b36c45c352aaddc29fcdc133010595126c4d
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-11-21 06:40:29 -0300

    Adequação do nome

--- src/main/java/com/osprasoft/faculdade/CursoApplication.java
+++ src/main/java/com/osprasoft/faculdade/CursoApplication.java
@@ -1,4 +1,4 @@
-package com.osprasoft.curso;
+package com.osprasoft.faculdade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
