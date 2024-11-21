119ceeb5249267785dc8db299a4dc2e2d8031346
Igor Minoru Tamura <igor.minoru@outlook.com>
2024-11-21 18:26:37 -0300

    Ajustes finais do projeto.

--- src/main/java/com/osprasoft/accentureacademy/resources/CursoResource.java
+++ src/main/java/com/osprasoft/accentureacademy/resources/CursoResource.java
@@ -1,9 +1,7 @@
 package com.osprasoft.accentureacademy.resources;
 
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.osprasoft.accentureacademy.dto.CursoDTO;
 import com.osprasoft.accentureacademy.dto.util.InscricaoPopulator;
 import com.osprasoft.accentureacademy.services.CursoService;
--- src/main/java/com/osprasoft/accentureacademy/resources/InscricaoResource.java
+++ src/main/java/com/osprasoft/accentureacademy/resources/InscricaoResource.java
@@ -2,18 +2,15 @@ package com.osprasoft.accentureacademy.resources;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.osprasoft.accentureacademy.dto.AlunoDTO;
 import com.osprasoft.accentureacademy.dto.CursoDTO;
 import com.osprasoft.accentureacademy.dto.InscricaoDTO;
